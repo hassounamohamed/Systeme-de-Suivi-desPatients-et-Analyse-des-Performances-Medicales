e706846a380384554bccf0ed9d030ec84c99e198
hassounamohamed <mohamed.hassouna.2004.24@gmail.com>
2025-01-20 12:25:15 +0100

    Side-nav And Admin Dashboard

---
+++ admin/sidenav.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<style>
+        /* Additional styling for sidenav */
+        .sidenav {
+            height: 90vh;
+            background-color: #17a2b8;
+        }
+        .sidenav a {
+            color: #17a2b8;
+        }
+        .sidenav a:hover {
+            color: white;
+            background-color: #138496;
+        }
+    </style>
+
+<div class="col-md-2 sidenav p-0">
+                <div class="list-group text-center">
+                    <a href="index.php" class="list-group-item list-group-item-action">Dashboard</a>
+                    <a href="administrators.php" class="list-group-item list-group-item-action">Administrators</a>
+                    <a href="doctors.php" class="list-group-item list-group-item-action">Doctors</a>
+                    <a href="patients.php" class="list-group-item list-group-item-action">Patients</a>
+                </div>
+            </div>
+    
+</body>
+</html>
+
+
+
+
+
+
